74eff3e221d0747ceb03d32d77682871a4289c23
saikumar22 <saigoud2212@gmail.com>
2020-08-19 10:03:14 +0530

    done

--- src/model/City.java
+++ src/model/City.java
@@ -1,1 +1,24 @@
+package model;
 
+public class City{
+	int cityid;
+	String cityname;
+	public int getCityid() {
+		return cityid;
+	}
+	public void setCityid(int cityid) {
+		this.cityid =  cityid;
+	}
+	public String getCityname() {
+		return cityname;
+	}
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+	public City(int cityid, String cityname) {
+		super();
+		this.cityid = cityid;
+		this.cityname = cityname;
+	}
+
+}
